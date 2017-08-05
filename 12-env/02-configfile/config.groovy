e26f5ff20edd91addd0c7f5b65f91541ca0f589f
F.W <pkaq@msn.com>
2017-08-05 15:45:27 +0800

    采用config文件的方式实现多环境打包

---
+++ 12-env/02-configfile/config.groovy
@@ -0,0 +1,18 @@
+environments {
+    // 开发环境
+    dev {
+        db {
+            username = "dev"
+            password = 'devpwd'
+        }        
+    }
+    // 线上环境
+    production { 
+        db {
+            username = "prod"
+            password = 'prodpwd'
+        }        
+    }
+}
+
+
