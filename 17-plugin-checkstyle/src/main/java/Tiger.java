179550b0fa7ce96a33e3ab07cc16896d38e22dc8
PKAQ <pkaq@msn.com>
2018-12-02 13:19:01 +0800

    代码风格检查

---
+++ 17-plugin-checkstyle/src/main/java/Tiger.java
@@ -0,0 +1,12 @@
+/**
+ * Tiger say hi
+ */
+public class Tiger {
+    public static void sayHi(){
+        System.out.println("Hi~");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("PKAQ");
+    }
+}
